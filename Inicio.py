915395de9f1e566b7e02d9517c18bf4443388ace
Alejandro Alvarez <92798860+Alejoab@users.noreply.github.com>
2022-11-21 13:25:44 -0500

    Agrega comentarios

--- Inicio.py
+++ Inicio.py
@@ -1,43 +1,55 @@
-from tkinter import Tk, Label, ttk, Button 
- 
-class Inicio(Tk): 
-    def __init__(self, aciertos, fallos): 
-        super().__init__() 
-        self.title('Wordle Equipo 8') 
-        self.aciertos = aciertos 
-        self.fallos = fallos 
-        self.minsize(1024, 640) 
- 
-        text = f'Aciertos: {aciertos}   Fallos: {fallos}'
-        titulo = Label(self, text = text, font = ('Helvetica', 24, 'bold')) 
-        titulo.pack(pady = 20) 
- 
-        titulo = Label(self, text = 'Wordle - Equipo #3', font = ('Helvetica', 24, 'bold')) 
-        titulo.pack(pady = 20) 
- 
-        titulo = Label(self, text = 'Por favor seleccione una dificultad', font = ('Helvetica', 12, 'bold')) 
-        titulo.pack(pady = 30) 
-         
-        self.combo = ttk.Combobox(self, values=["4","5","6","7","8"], state='readOnly', ) 
-        self.combo.set("4") 
-        self.combo.pack() 
- 
-        boton1 = Button(self, text = "Continuar") 
-        boton1.pack(anchor = 'c', pady=20) 
-        boton1.bind("<Button-1>", self.crearJuego) 
- 
-    def crearJuego(self, evento): 
-        from Juego import Juego 
-        dificultad = self.combo.get() 
-        self.destroy() 
-        ventana = Juego(dificultad, self.aciertos, self.fallos) 
-        ventana.mainloop() 
- 
-         
- 
- 
- 
-if __name__ == '__main__': 
-    ventana = Inicio(0,0) 
-    ventana.mainloop() 
-    
+from tkinter import Tk, Label, ttk, Button, Frame
+
+
+class Inicio(Tk):
+    def __init__(self, aciertos, fallos):
+        # Configuracion basica de la pantall
+        super().__init__()
+        self.title('Wordle Equipo 8')
+        self.minsize(960, 540)
+
+        # Se guarda la cantidad de aciertos y de fallos, luego se presentan en la ventana
+        self.aciertos = aciertos
+        self.fallos = fallos
+        text = f'Aciertos: {aciertos}              Fallos: {fallos}'
+        Label(self, text=text, font=('Helvetica', 15, 'bold')).pack(
+            pady=10, padx=20, anchor='w')
+
+        # Se pone el titulo principal del Juego
+        Label(self, text='Wordle - Equipo #8',
+              font=('Helvetica', 24, 'bold')).pack(pady=20)
+
+        # Seleccion de la dificultad del juego
+        Label(self, text='Por favor seleccione una dificultad',
+              font=('Helvetica', 12, 'bold')).pack(pady=30)
+
+        # Combobox para la seleccion de la dificultad
+        self.combo = ttk.Combobox(
+            self, values=["4", "5", "6", "7", "8"], state='readOnly', height=2, width=15, font=('Helvetica', 11, 'bold'))
+        self.combo.set("4")
+        self.combo.pack()
+
+        # Boton para iniciar el juego
+        boton1 = Button(self, text="Continuar", bg='dodger blue', height=2,
+                        width=10, font=('Helvetica', 11, 'bold'), fg='white')
+        boton1.pack(anchor='c', pady=20)
+        boton1.bind("<Button-1>", self.crearJuego)
+
+    def crearJuego(self, evento):
+        '''Funcion para inicializar la ventana de creacion del juego'''
+        from Juego import Juego
+
+        # Se obtiene la dificultad seleccionada por el usuario
+        dificultad = self.combo.get()
+
+        # Se destruye la ventana actual
+        self.destroy()
+
+        # Se inicializa la ventana del juego
+        ventana = Juego(dificultad, self.aciertos, self.fallos)
+        ventana.mainloop()
+
+
+if __name__ == '__main__':
+    ventana = Inicio(0, 0)
+    ventana.mainloop()
--- Juego.py
+++ Juego.py
@@ -1,62 +1,86 @@
-from tkinter import Tk, Label, ttk, Button, Frame, Entry, messagebox, CENTER
+from tkinter import Tk, Label, ttk, Button, Frame, Entry, messagebox, CENTER, W
 import words
 from random import choice
- 
+
+
 class Juego(Tk):
     colores = {'gris': '#787c7e', 'amarillo': '#c9b458', 'verde': '#6aaa64'}
-    def __init__(self, dificultad, aciertos, fallos): 
-        super().__init__() 
-        self.title('Wordle Equipo 8') 
-        self.minsize(1024, 640) 
- 
-        self.aciertos = aciertos 
-        self.fallos = fallos 
+
+    def __init__(self, dificultad, aciertos, fallos):
+        # Configuracion basica de la pantalla
+        super().__init__()
+        self.title('Wordle Equipo 8')
+
+        # Pantalla completa
+        self.minsize(1024, 700)
+
+        # Se guardan caracteristicas basicas de la partida
+        self.aciertos = aciertos
+        self.fallos = fallos
         self.dificultad = dificultad
-        self.gana = False 
-        self.intento = 1 
- 
-        text = f'Aciertos: {aciertos}   Fallos: {fallos}' 
-        titulo = Label(self, text = text, font = ('Helvetica', 24, 'bold')) 
-        titulo.pack(pady = 20) 
+        self.gana = False
+        self.intento = 1
+
+        # Se muestran los aciertos y fallos
+        text = f'Aciertos: {aciertos}              Fallos: {fallos}'
+        Label(self, text=text, font=('Helvetica', 15, 'bold')).pack(
+            pady=10, padx=20, anchor='w')
+
+        # Se escoge una palabra al azar
         self.palabra_escogida = self.escoger()
-        self.ejecutar() 
- 
-    def escoger(self): 
+        print(self.palabra_escogida)
+
+        self.ejecutar()
+
+    def escoger(self):
         return choice(words.palabras_dict[self.dificultad])
- 
-    def ejecutar(self): 
-        # Elementos de la ventana 
-        titulo = Label(self, text = 'Escribe la palabra:', font = ('Helvetica', 16, 'bold')) 
-        titulo.pack(pady = 20) 
- 
-        self.input = Entry(self, font =('Helvetica', 12)) 
-        self.input.pack(fill='x', padx = 50) 
- 
-        # Botón para hacer un intento 
-        boton_intento = Button(self, text = 'Intentar', font = ('Helvetica', 12, 'bold'), bg='dodger blue', fg='white') 
-        boton_intento.pack(pady = 10) 
-        boton_intento.bind("<Button-1>", self.dibujar_palabra) 
- 
-        # Espacio donde saldrán los resultados 
-        self.frame_intento = Frame(self, width = 400, height=400)
-        self.frame_intento.pack(pady = 10) 
- 
-        # Botón para hacer un intento 
-        boton_termina = Button(self, text = 'Terminar partida', font = ('Helvetica', 12, 'bold'), bg='red3', fg='white') 
-        boton_termina.pack(pady = 10) 
-        boton_termina.bind("<Button-1>", self.otra_partida) 
- 
+
+    def ejecutar(self):
+        # Elementos de la ventana
+        Label(self, text='Escribe la palabra:',
+              font=('Helvetica', 16, 'bold')).pack(pady=10)
+
+        self.input = Entry(self, font=('Helvetica', 12),
+                           justify='center', width=50)
+        self.input.pack(ipady=3)
+
+        # Botón para hacer un intento
+        boton_intento = Button(self, text='Intentar', font=(
+            'Helvetica', 12, 'bold'), bg='dodger blue', fg='white', activebackground='dodger blue', activeforeground='white', command=lambda: self.dibujar_palabra())
+        boton_intento.pack(pady=10)
+
+        # Espacio donde saldrán los resultados
+        self.frame_intento = Frame(self)
+        self.frame_intento.pack(pady=10)
+
+        # Botón para hacer un intento
+        boton_termina = Button(self, text='Terminar partida', font=(
+            'Helvetica', 12, 'bold'), bg='red3', fg='white', activebackground='red3', activeforeground='white', command=lambda: self.otra_partida())
+        boton_termina.pack(pady=10)
+
     def procesar_palabra(self, palabra_ingresada):
+        '''Funcion para determinar los colores de cada letra teniendo en cuenta la palabra escogida'''
+        # Se van agregando los colores de cada letra
         resultados_palabra = []
+        # Sirve para determinar la cantidad de letras amarillas que se han agregado
         contador_amarillas = {}
+        #
+
+        # Se itera sobre el tamaño de la palabra
         for i in range(int(self.dificultad)):
+
+            # Si la letra de la palabra ingresada se encuentra en la palabra escogida, en caso contrario se agrega el color gris a la lista
             if palabra_ingresada[i] in self.palabra_escogida:
+                # Si la letra es igual a la letra que esta en la misma posicion de la palabra elegida
                 if self.palabra_escogida[i] == palabra_ingresada[i]:
                     resultados_palabra.append('verde')
                 else:
+                    # Si es la primera vez que se encuentra esa letra en la palabra escogida
                     if palabra_ingresada[i] not in contador_amarillas:
                         contador_amarillas[palabra_ingresada[i]] = 1
                         resultados_palabra.append('amarillo')
+
+                    # Si todavia se pueden pintar mas letras de color amarillo
                     elif contador_amarillas[palabra_ingresada[i]] < self.palabra_escogida.count(palabra_ingresada[i]):
                         contador_amarillas[palabra_ingresada[i]] += 1
                         resultados_palabra.append('amarillo')
@@ -64,53 +88,74 @@ class Juego(Tk):
                         resultados_palabra.append('gris')
             else:
                 resultados_palabra.append('gris')
+
         return resultados_palabra
 
-    def dibujar_palabra(self, evento): 
-        palabra_ingresada = self.input.get() 
+    def dibujar_palabra(self):
+        '''Funcion para dibujar la palabra y ingresada y el color de cada letra'''
+        # Obtener palabra y limpiar input
+        palabra_ingresada = self.input.get().lower()
         self.input.delete(0, 'end')
-              
+
+        # Si la palabra es más grande o más pequeña se le pide que ingrese una nueva palabra
         if len(palabra_ingresada) != int(self.dificultad):
-            messagebox.showinfo(title="Error",message=f'La palabra que ingresó es no válida. Por favor ingrese una palabra de tamaño {self.dificultad}') 
-            return 
-        if palabra_ingresada not in words.palabras_set:
-            messagebox.showinfo(title="Error",message='La palabra que ingresó es no válida. Por favor ingrese una palabra existente.') 
-            return 
-        if palabra_ingresada == self.palabra_escogida: 
-            for i in range(int(self.dificultad)): 
-                celda = Label(self.frame_intento, text = palabra_ingresada[i].upper(), width = 6, height = 3, bg = Juego.colores['verde'], fg='white', font=('Helvetica', 12, 'bold')) 
-                celda.grid(row = self.intento, column = i, padx = 2, pady = 2)
-                
-            messagebox.showinfo(message="¡¡Ganaste!!", title="Partida terminada") 
-            self.gana = True 
-            self.otra_partida() 
-            return 
-        
-        color_palabra = self.procesar_palabra(palabra_ingresada)
-        
- 
-        for i in range(int(self.dificultad)): 
- 
-            celda = Label(self.frame_intento, text = palabra_ingresada[i].upper(), width = 6, height = 3, bg = Juego.colores[color_palabra[i]], fg='white', font=('Helvetica', 12, 'bold')) 
-            celda.grid(row = self.intento, column = i, padx = 2, pady = 2) 
- 
-        self.intento += 1 
- 
-        if self.intento == 7: 
-            messagebox.showinfo(message=f"Perdiste", title=f"Partida terminada. La palabra correcta es {self.palabra_escogida}") 
-            self.otra_partida() 
+            messagebox.showinfo(
+                title="Error", message=f'La palabra que ingresó es no válida. Por favor ingrese una palabra de tamaño {self.dificultad}')
             return
-                
-    def otra_partida(self, evento = None): 
-        from Inicio import Inicio 
-        self.destroy() 
-        if self.gana: 
-            ventana = Inicio(self.aciertos+1, self.fallos) 
-        else: 
-            ventana = Inicio(self.aciertos, self.fallos+1) 
-        ventana.mainloop()     
- 
- 
-if __name__ == '__main__': 
-    ventana = Juego(4,0,0) 
-    ventana.mainloop()
+
+        # Si la palabra ingresada no está en el lemario elegido
+        if palabra_ingresada not in words.palabras_set:
+            messagebox.showinfo(
+                title="Error", message='La palabra que ingresó es no válida. Por favor ingrese una palabra existente.')
+            return
+
+        # Si la palabra ingresada corresponde a la palabra elegida
+        if palabra_ingresada == self.palabra_escogida:
+
+            # Poner toda la palabra en verde
+            for i in range(int(self.dificultad)):
+                celda = Label(self.frame_intento, text=palabra_ingresada[i].upper(
+                ), width=6, height=3, bg=Juego.colores['verde'], fg='white', font=('Helvetica', 12, 'bold'))
+                celda.grid(row=self.intento, column=i, padx=2, pady=2)
+
+            # Mostrar mensaje de que gano la partida
+            messagebox.showinfo(message="¡Ganaste!",
+                                title="Partida terminada")
+
+            # Gana la partida y lo envia a la pantalla de inicio para que seleccione nuevamente la dificultad
+            self.gana = True
+            self.otra_partida()
+            return
+
+        # Retorna los colores de cada letra de la palabra
+        color_palabra = self.procesar_palabra(palabra_ingresada)
+
+        for i in range(int(self.dificultad)):
+            #
+            celda = Label(self.frame_intento, text=palabra_ingresada[i].upper(
+            ), width=6, height=3, bg=Juego.colores[color_palabra[i]], fg='white', font=('Helvetica', 12, 'bold'))
+            celda.grid(row=self.intento, column=i, padx=2, pady=2)
+
+        # Se aumenta un intento
+        self.intento += 1
+
+        # Si ya completo todos lo intentos muestra mensaje de error
+        if self.intento == 7:
+            messagebox.showinfo(
+                message=f"Partida terminada. La palabra correcta es {self.palabra_escogida}", title=f'Perdiste')
+            self.otra_partida()
+            return
+
+    def otra_partida(self):
+        '''Funcion para jugar otra partida'''
+        from Inicio import Inicio
+        # Destruye la ventana actual
+        self.destroy()
+
+        # Si gano, aumenta unoa los aciertos, si pierde aumenta uno a los fallos
+        if self.gana:
+            ventana = Inicio(self.aciertos+1, self.fallos)
+        else:
+            ventana = Inicio(self.aciertos, self.fallos+1)
+
+        ventana.mainloop()
